4d1434f607b81d48e73e414070d6d74853e36d33
norkitorki <norki@tuta.io>
2022-06-07 09:09:18 +0200

    Generate user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  
+  validates_associated :posts
+  validates_associated :comments
+
+  validates :username, :password, :birthdate, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 8 }
+end
