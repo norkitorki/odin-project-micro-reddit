5882b249ca10b5006e1f54a8cac8e4ad966f2ff1
norkitorki <norki@tuta.io>
2022-06-08 11:56:09 +0200

    Update schema with renamed posts url column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_052212) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_053616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_052212) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.string "url"
+    t.string "post_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
