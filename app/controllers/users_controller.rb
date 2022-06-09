e61de23ed62fa585314277c8eb1ead39a7ab6902
norkitorki <norki@tuta.io>
2022-06-09 14:11:29 +0200

    Pass @user and @contributions to show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @contributions = (@user.posts + @user.comments).sort { |a, b| b.created_at <=> a.created_at }
   end
 end
