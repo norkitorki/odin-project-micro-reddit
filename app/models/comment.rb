2f6fc91590b80fd26c2ef36e9b60a50c650ff222
norkitorki <norki@tuta.io>
2022-06-09 07:51:53 +0200

    Add after_remove callback to delete orphaned comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,15 @@
 class Comment < ApplicationRecord
-  has_many :comments, class_name: "Comment", foreign_key: "parent_comment_id"
+  has_many :comments, class_name: "Comment", foreign_key: "parent_comment_id", after_remove: :delete_orphaned_comments
   
   belongs_to :user
   belongs_to :post
 
   validates :author, :body, presence: true
   validates :body, length: { maximum: 1000 }
+
+  private
+
+  def delete_orphaned_comments
+    Comment.all.select { |c| c.parent_comment_id && !Comment.find_by(id: c.parent_comment_id) }.delete_all
+  end
 end
