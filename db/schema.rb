961e3eee552fdf7be6845f95645abb327ff70bad
norkitorki <norki@tuta.io>
2022-06-10 17:18:01 +0200

    Create rating column on comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_103830) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_114017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_103830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "parent_comment_id"
+    t.integer "rating", default: 0
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
