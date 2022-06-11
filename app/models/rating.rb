e3aa13561dd70e5cdded599329007c94ac2d362b
norkitorki <norki@tuta.io>
2022-06-11 15:59:39 +0200

    Add format_rating method to rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :rateable, polymorphic: true
+  def format_rating
+    rating = value.to_i
+    rating >= 1000 ? "#{(rating / 1000.0).round(1).to_s.sub('.0', '')}k" : rating.to_s
+  end
 end
