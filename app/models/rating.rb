411ce6d2055da3269f96701e54b8144bdae24dcf
norkitorki <norki@tuta.io>
2022-06-11 15:59:58 +0200

    Add update_rating to rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,10 @@
 class Rating < ApplicationRecord
   belongs_to :rateable, polymorphic: true
+
+  def update_rating(num)
+    update(value: value + num)
+  end
+
   def format_rating
     rating = value.to_i
     rating >= 1000 ? "#{(rating / 1000.0).round(1).to_s.sub('.0', '')}k" : rating.to_s
