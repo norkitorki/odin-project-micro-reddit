23a41d6f35dd1516e947b86330cf9fc545792f10
norkitorki <norki@tuta.io>
2022-06-11 15:16:37 +0200

    Refactor format_rating by requiring rating as parameter

--- app/models/concerns/rateable.rb
+++ app/models/concerns/rateable.rb
@@ -1,8 +1,8 @@
 module Rateable
   extend ActiveSupport::Concern
 
-  def format_rating
-    rating = self.rating || 0
+  def format_rating(rating)
+    rating = rating.to_i
     rating >= 1000 ? "#{(rating / 1000.0).round(1).to_s.sub('.0', '')}k" : rating.to_s
   end
 end
