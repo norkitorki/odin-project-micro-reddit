71dafccc25b6c124e8f74c8f4b21482ce091bb29
norkitorki <norki@tuta.io>
2022-06-11 16:26:17 +0200

    Delete concerns/rateable.rb

--- app/models/concerns/rateable.rb
+++
@@ -1,8 +0,0 @@
-module Rateable
-  extend ActiveSupport::Concern
-
-  def format_rating(rating)
-    rating = rating.to_i
-    rating >= 1000 ? "#{(rating / 1000.0).round(1).to_s.sub('.0', '')}k" : rating.to_s
-  end
-end
