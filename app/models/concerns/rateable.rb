b970a40850810d11b625bb27cedd60351b72cb12
norkitorki <norki@tuta.io>
2022-06-11 07:22:58 +0200

    Modify format_rating to handle trailing zeroes

--- app/models/concerns/rateable.rb
+++ app/models/concerns/rateable.rb
@@ -3,6 +3,6 @@ module Rateable
 
   def format_rating
     rating = self.rating || 0
-    rating >= 1000 ? "#{(rating / 1000.0).round(1)}k" : rating.to_s
+    rating >= 1000 ? "#{(rating / 1000.0).round(1).to_s.sub('.0', '')}k" : rating.to_s
   end
 end
